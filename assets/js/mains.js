925aa04c0f0097cd4c37d2ceacb8c8cdf6762dc6
Akshay <akshayvgupta7@gmail.com>
2024-07-26 09:56:51 +0530

    try catch

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -2,6 +2,7 @@ import { categoryData, productData } from "./api";
 
 
 const productService = async()=>{
+    try{
     let products;
     let categories = await categoryData("https://fakestoreapi.com/products/categories");
     categories = await categories?.json();
@@ -14,6 +15,9 @@ const productService = async()=>{
          return {products,categories}
     }
     return categories;
+    }catch(error){
+        console.log("err:",error)
+    }
 }
 
 console.log(productService());
