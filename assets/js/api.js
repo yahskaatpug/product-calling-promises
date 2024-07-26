eedd970c06488aa53619a2c47df526e277c89cea
Akshay <akshayvgupta7@gmail.com>
2024-07-26 11:42:06 +0530

    api timeout added

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,17 +1,35 @@
-export const categoryData = async (url) => {
+export const categoryData = async (url, timeOut) => {
+    const {controller, timeOutId} = apiTimeOut();
+
     try {
-        const category = await fetch(url);
+        const category = await fetch(url, {signal: controller.signal});
+        clearTimeout(timeOutId);
         return category.json();
     } catch (err) {
-
+        console.log("error:", err);
+        if(error.name === 'AbortError') {
+            throw new Error('Request timed out');
+        }
     }
 }
 
 export const productData = async (url) => {
+   const {controller, timeOutId} = apiTimeOut();
+   clearTimeout(timeOutId);
     try {
-        const product = await fetch(url);
+        const product = await fetch(url,  {signal: controller.signal});
         return product.json();
     } catch (err) {
-
+        console.log("error:", err);
+        if(error.name === 'AbortError') {
+            throw new Error('Request timed out');
+        }
     }
+}
+
+const apiTimeOut = ()=>{
+    const controller = new AbortController();
+    const abortController = ()=> controller.abort();
+    const timeOutId = setTimeout(()=> (abortController, 1000));
+    return {controller, timeOutId}
 }
--- assets/js/mains.js
+++ assets/js/mains.js
@@ -4,11 +4,11 @@ import { categoryData, productData } from "./api.js";
 const productService = async () => {
     try {
         let products;
-        const categories = await categoryData("https://fakestoreapi.com/products/categories");
+        const categories = await categoryData("https://fakestoreapi.com/products/categories",1000);
         console.log("categories:", categories);
 
         if (categories) {
-            products = await productData("https://fakestoreapi.com/products");
+            products = await productData("https://fakestoreapi.com/products",1000);
             console.log("products:", products);
             return { products, categories }
         }
